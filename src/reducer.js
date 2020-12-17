a660ac4c205b279af456e8fe6d0a696ab5576150
Sune Carelse <sune.carelse@takealot.com>
2020-12-17 20:55:08 +0200

    using the DataLayer and reducer

--- src/reducer.js
+++ src/reducer.js
@@ -3,4 +3,20 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null
-}
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch(action.type) {
+    case 'SET_USER':
+        return {
+            ...state,
+            user: action.user,
+        };
+    default:
+        return state;
+  }
+};
+
+export default reducer;
