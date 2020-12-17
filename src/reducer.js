a71c5adf2c5b2dd6e475ef1153fc7e140befc1ae
Sune Carelse <sune.carelse@takealot.com>
2020-12-17 21:50:00 +0200

    created the player components (sidebar/body)

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,9 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
+    item: null,
+    // Remove after finished developing...
+    token: 'BQBgaydMMzgZTlSMYhXF0aSn-Je4X2L7oxnX92u_wFdVd3x_kiZ3wGRAAcPV7P7BOH1SVGQazWxlji56ed-fq3ZqaSR1upN3xYJEUr5qhZesNO84WjZHNTTnzuWfQUJdVRI3csZ5Y35JtMKqohNO7GV-TTzJrxeN',
 };
 
 const reducer = (state, action) => {
@@ -14,6 +16,12 @@ const reducer = (state, action) => {
             ...state,
             user: action.user,
         };
+
+    case 'SET_TOKEN':
+        return {
+            ...state,
+            token: action.token
+        }
     default:
         return state;
   }
