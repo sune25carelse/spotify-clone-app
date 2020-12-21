c0bf3a1b8aef07c06f4cbb8fcb1987c4b0597ee3
Sune Carelse <sune.carelse@takealot.com>
2020-12-21 22:51:33 +0200

    created icons and playlist and styling for sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import { useDataLayerValue } from './DataLayer';
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue()
+
   return (
     <div className="sidebar">
       <img 
@@ -10,9 +16,18 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
         alt="" 
       />
-      <SidebarOption title="Home"/>
-      <SidebarOption title="Search"/>
-      <SidebarOption title="Your Library"/>
+      <SidebarOption Icon={HomeIcon} title="Home"/>
+      <SidebarOption Icon={SearchIcon} title="Search"/>
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOption title='Hip Hop' />
+      <SidebarOption title='Rock' />
+      <SidebarOption title='R&B' />
+
     </div>
   );
 }
