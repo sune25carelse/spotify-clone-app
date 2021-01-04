469758a6dd54444877bc3f79d252f660c1a9cfd0
Sune Carelse <sune.carelse@takealot.com>
2021-01-05 00:06:29 +0200

    creating the SonRow and body icons and style

--- src/Body.js
+++ src/Body.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import './Body.css'; 
+import { useDataLayerValue } from './DataLayer';
 import Header from './Header';
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz"; 
+import SongRow from './SongRow';
 
 function Body({ spotify }) {
+  const [{ discover_weekly, dispatch } ] = useDataLayerValue();
+
   return (
     <div className='body'>
       <Header spotify={spotify} />
 
       <div className="body__info">
         <img
-          src="https://i.scdn.co/image/11258b6c69204820d79575e0587f415735db2350"
+          src={discover_weekly?.images[0].url}
           alt=""
         />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+         <PlayCircleFilledIcon className="body__shuffle"/>
+          <FavoriteIcon />
+          <MoreHorizIcon />
+        </div>
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
