41ab68f7215e06c28a010647df5fdb3887d0f72b
Sune Carelse <sune.carelse@takealot.com>
2021-01-04 22:11:22 +0200

    body banner created pic and descriptn

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,18 @@ function Body({ spotify }) {
   return (
     <div className='body'>
       <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img
+          src="https://i.scdn.co/image/11258b6c69204820d79575e0587f415735db2350"
+          alt=""
+        />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>description...</p>
+        </div>
+      </div>
     </div>
   );
 }
